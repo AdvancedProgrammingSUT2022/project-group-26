4db9615fe2714a7f7fd95bc8db2283b4b06b1e6d
pariyahr <pariyahajipour@gmail.com>
2022-05-09 17:15:20 +0430

    70% game map test

--- src/test/java/models/GameMapTest.java
+++ src/test/java/models/GameMapTest.java
@@ -1,5 +1,6 @@
 package models;
 
+import models.Tile.Tile;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ public class GameMapTest {
     private GameMap gameMap;
     private Player player;
     private User user;
+    private Tile tile;
+    Tile[][] tiles = new Tile[30][30];
 
     private ArrayList<Player> players = new ArrayList<>();
 
@@ -17,11 +20,24 @@ public class GameMapTest {
     public void setUp() {
         player = new Player(user);
         players.add(player);
+        gameMap = new GameMap(tiles);
     }
 
     @Test
-    public void lj() {
+    public void initialGameMapTest() {
         gameMap = new GameMap(players);
         Assertions.assertEquals(null, players.get(0).getGameMap().getTile(0, 0));
     }
+
+    @Test
+    public void getIndexITest(){
+        Assertions.assertEquals(0, gameMap.getIndexI(gameMap.getTile(0,0)));
+    }
+
+    @Test
+    public void getIndexJTest(){
+        Assertions.assertEquals(0, gameMap.getIndexJ(gameMap.getTile(0,0)));
+    }
+
+
 }
