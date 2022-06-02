2bd07e9a90b8c1c9dee119eeb67a1f01624bc927
Ali Aghayari <ali.aghayari@hotmail.com>
2022-06-02 18:24:47 +0430

    finished 2 menus

--- src/main/java/com/example/project/views/LoginPage.java
+++ src/main/java/com/example/project/views/LoginPage.java
@@ -2,6 +2,9 @@ package com.example.project.views;
 
 import com.example.project.controllers.LoginMenuController;
 import com.example.project.controllers.Output;
+import com.example.project.models.DataBase;
+import com.example.project.models.User;
+import com.example.project.models.UsersDatabase;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
@@ -19,6 +22,8 @@ import java.util.Objects;
 
 public class LoginPage {
     LoginMenuController loginMenuController = new LoginMenuController();
+    UsersDatabase usersDatabase = UsersDatabase.getInstance();
+    DataBase dataBase = DataBase.getInstance();
 
     @FXML
     private TextField usernameFieldSignUp;
@@ -47,24 +52,20 @@ public class LoginPage {
     @FXML
     public void initialize() {
         usernameFieldSignUp.setOnKeyPressed(k -> {
-            if (k.getCode().equals(KeyCode.ENTER)) registerUser();
+            if (k.getCode().equals(KeyCode.ENTER)) nicknameFieldSignUp.requestFocus();
         });
         nicknameFieldSignUp.setOnKeyPressed(k -> {
-            if (k.getCode().equals(KeyCode.ENTER)) registerUser();
+            if (k.getCode().equals(KeyCode.ENTER)) passwordFieldSignUp.requestFocus();
         });
         passwordFieldSignUp.setOnKeyPressed(k -> {
-            if (k.getCode().equals(KeyCode.ENTER)) registerUser();
+            if (k.getCode().equals(KeyCode.ENTER)) secondPasswordField.requestFocus();
         });
         secondPasswordField.setOnKeyPressed(k -> {
             if (k.getCode().equals(KeyCode.ENTER)) registerUser();
         });
         usernameFieldLogin.setOnKeyPressed(k -> {
             if (k.getCode().equals(KeyCode.ENTER)) {
-                try {
-                    loginUser();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+                passwordFieldLogin.requestFocus();
             }
         });
         passwordFieldLogin.setOnKeyPressed(k -> {
@@ -79,9 +80,11 @@ public class LoginPage {
     }
 
     public void registerUser() {
-        Output massage = loginMenuController.register(usernameFieldSignUp.getText(), nicknameFieldSignUp.getText(), passwordFieldSignUp.getText(), secondPasswordField.getText());
-        // pop up
-        // do the thing
+        Output message = loginMenuController.register(usernameFieldSignUp.getText(), nicknameFieldSignUp.getText(), passwordFieldSignUp.getText(), secondPasswordField.getText());
+        // todo : should handel error and info!
+        new PopupMessage(Alert.AlertType.ERROR, message.toString());
+        if (message == Output.REGISTERED)
+            usersDatabase.getUsers().add(new User(usernameFieldSignUp.getText(), passwordFieldSignUp.getText(), nicknameFieldSignUp.getText()));
         usernameFieldSignUp.clear();
         nicknameFieldSignUp.clear();
         passwordFieldSignUp.clear();
@@ -90,14 +93,17 @@ public class LoginPage {
 
     public void loginUser() {
         Output message = loginMenuController.login(usernameFieldLogin.getText(), passwordFieldLogin.getText());
-        // pop up
-        // do the thing
+        // todo : should handel error and info!
+        new PopupMessage(Alert.AlertType.ERROR, message.toString());
+        if (message == Output.LOGGED_IN) {
+            DataBase.getInstance().setLoggedInUser(UsersDatabase.getInstance().getUserByUsername(usernameFieldLogin.getText()));
+            MenuChanger.changeMenu("MainMenu");
+        }
         usernameFieldLogin.clear();
         passwordFieldLogin.clear();
     }
 
     public void exit(MouseEvent mouseEvent) {
-        // pop up -- confirmation
         Platform.exit();
     }
 
