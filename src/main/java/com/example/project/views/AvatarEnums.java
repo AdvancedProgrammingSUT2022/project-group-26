cc123fdbd20646ce52b0a47502339c0e5c1b2a8c
Ali Aghayari <ali.aghayari@hotmail.com>
2022-06-03 20:29:40 +0430

    save for now

---
+++ src/main/java/com/example/project/views/AvatarEnums.java
@@ -0,0 +1,13 @@
+package com.example.project.views;
+
+public enum AvatarEnums {
+    AVATAR1(""),
+    AVATAR2(""),
+    AVATAR3(""),
+    AVATAR4(""),
+    ;
+
+    AvatarEnums(String s) {
+
+    }
+}
