4de1f420829bbb00098f847b3f333a058b1ca1f7
Ali Aghayari <ali.aghayari@hotmail.com>
2022-07-11 22:49:03 +0430

    started gamePanel-dataBar

--- src/main/java/com/example/project/views/ShowPanelFXController.java
+++ src/main/java/com/example/project/views/ShowPanelFXController.java
@@ -1,14 +1,15 @@
 package com.example.project.views;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -49,16 +50,68 @@ public class ShowPanelFXController {
 
     public void setupPanels() {
         setupStatusBar();
-        // todo : setups
+        setupDataBar();
+        setupSelectedObjectData();
+    }
+
+    private void setupSelectedObjectData() {
+        // hbox
+        // in it circle and hbox
+        HBox objectData = new HBox();
+
+        objectData.setPrefWidth(700);
+        objectData.setPrefHeight(100);
+        objectData.setLayoutY(650);
+        objectData.setPadding(new Insets(15, 12, 15, 12));
+        objectData.setSpacing(10);
+        objectData.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
+
+        Circle researchImage = new Circle();
+        researchImage.setRadius(90);
+        researchImage.setFill(Color.BLUEVIOLET);
+
+        VBox data = new VBox();
+
+        Label name = new Label();
+//        Label health = new Label();
+//        Label attack = new Label();
+//        Label name = new Label();
+        data.getChildren().addAll();
+        objectData.getChildren().addAll(researchImage, data);
+
+        pane.getChildren().add(objectData);
+
+    }
+
+    private void setupDataBar() {
+        VBox dataBar = new VBox();
+
+        dataBar.setPrefWidth(90);
+        dataBar.setPrefHeight(820);
+        dataBar.setPadding(new Insets(15, 12, 15, 12));
+        dataBar.setSpacing(10);
+        dataBar.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
+
+
+        // todo : add a hbox -> in it : circle and shit
+        Circle researchImage = new Circle();
+        researchImage.setRadius(30); // ??!
+        researchImage.setFill(Color.BLUEVIOLET);
+//        researchImage.setFill(new ImagePattern(new Image(new FileInputStream("src of the research"))));
+
+        // add a custom vbox that fills and erases ?!
+
     }
 
     private void setupStatusBar() {
         HBox statusBar = new HBox();
 
-        statusBar.setPrefWidth(1530);
-        statusBar.setPrefHeight(40);
-        statusBar.setSpacing(5);
-        statusBar.setBackground(new Background(new BackgroundFill(Color.BLACK, null, null)));
+        statusBar.setPrefWidth(1600);
+        statusBar.setPrefHeight(30);
+        statusBar.setPadding(new Insets(15, 12, 15, 12));
+        statusBar.setSpacing(10);
+        statusBar.setBackground(new Background(new BackgroundFill(Color.TRANSPARENT, null, null)));
+
 
         ImageView goldImage = null;
         ImageView happinessImage = null;
@@ -71,12 +124,31 @@ public class ShowPanelFXController {
             e.printStackTrace();
         }
 
-        goldImage.setFitHeight(25);
-        goldImage.setFitHeight(25);
-        happinessImage.setFitHeight(25);
-        happinessImage.setFitHeight(25);
-        scienceImage.setFitHeight(25);
-        scienceImage.setFitHeight(25);
+        goldImage.setFitHeight(30);
+        goldImage.setFitWidth(30);
+        happinessImage.setFitHeight(30);
+        happinessImage.setFitWidth(30);
+        scienceImage.setFitHeight(30);
+        scienceImage.setFitWidth(30);
+
+        goldAmount.setTextFill(Color.GOLD);
+        happinessAmount.setTextFill(Color.DARKGRAY);
+        scienceAmount.setTextFill(Color.DARKVIOLET);
+
+        goldAmount.setPrefHeight(25);
+        goldAmount.setPrefWidth(75);
+        happinessAmount.setPrefHeight(25);
+        happinessAmount.setPrefWidth(75);
+        scienceAmount.setPrefHeight(25);
+        scienceAmount.setPrefWidth(75);
+
+        goldAmount.setText("random");
+        happinessAmount.setText("random");
+        scienceAmount.setText("random");
+
+        goldAmount.setFont(new Font(15));
+        happinessAmount.setFont(new Font(15));
+        scienceAmount.setFont(new Font(15));
 
         statusBar.getChildren().addAll(goldImage, goldAmount, happinessImage, happinessAmount, scienceImage, scienceAmount);
 
