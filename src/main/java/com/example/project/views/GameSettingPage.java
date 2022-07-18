9ad416f356bda798092dfebeae162102a195e9e0
pariyahr <pariyahajipour@gmail.com>
2022-07-18 12:47:51 +0430

    remove sliders from game settings

--- src/main/java/com/example/project/views/GameSettingPage.java
+++ src/main/java/com/example/project/views/GameSettingPage.java
@@ -9,12 +9,8 @@ import javafx.util.Duration;
 
 public class GameSettingPage {
     public Slider saveNumber;
-    public Slider xMap;
-    public Slider yMap;
     public Slider playersNumber;
     public Label playerNumLabel;
-    public Label yMapLabel;
-    public Label xMapLabel;
     public Label saveNumLabel;
     public ChoiceBox savingChoiceBox;
     public Label savedFilesLabel;
@@ -22,8 +18,6 @@ public class GameSettingPage {
 
     public void initialize() {
         playersNumber.setTooltip(new Tooltip("set your preferred number of players"));
-        xMapLabel.setTooltip(new Tooltip("set your preferred number of horizontal tiles"));
-        yMapLabel.setTooltip(new Tooltip("set your preferred number of vertical tiles"));
         autoSaveCheck.setTooltip(new Tooltip("enable auto save"));
         saveNumber.setTooltip(new Tooltip("set your preferred max number of saved files"));
         savingChoiceBox.setTooltip(new Tooltip("set saving periods"));
@@ -35,8 +29,6 @@ public class GameSettingPage {
                 new KeyFrame(Duration.millis(1),
                         event -> {
                             playerNumLabel.setText(String.valueOf((int) playersNumber.getValue()));
-                            xMapLabel.setText(String.valueOf((int) xMap.getValue() * 10));
-                            yMapLabel.setText(String.valueOf((int) yMap.getValue() * 10));
                             saveNumLabel.setText(String.valueOf((int) saveNumber.getValue()));
                         }
                 )
